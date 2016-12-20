99465e9f2552542814718585ba8ffc7355020ab1
Leo <Leo3418@users.noreply.github.com>
2016-12-20 08:00:54 +0800

    ResultActivity.java: Better Back button
    When users click Back button, Android no longer shows the same animation
    as the app creates a new activity.

--- app/src/main/java/io/github/leo3418/ctscan/ResultActivity.java
+++ app/src/main/java/io/github/leo3418/ctscan/ResultActivity.java
@@ -117,6 +117,11 @@ public class ResultActivity extends AppCompatActivity {
                 // Display "About" box
                 about.show();
                 return true;
+            case android.R.id.home:
+                Intent intent = new Intent(this, MainActivity.class);
+                intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+                startActivity(intent);
+                return true;
             default:
                 return super.onOptionsItemSelected(item);
         }
